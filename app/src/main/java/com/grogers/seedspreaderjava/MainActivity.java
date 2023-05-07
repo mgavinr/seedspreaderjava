3574ec8b4a245dfc0c10b6985d5520edd988ac0d
Gavin Rogers <mgavinr@gmail.com>
2023-05-07 22:04:47 +0100

    Adding the buttons again for some reason

--- app/src/main/java/com/grogers/seedspreaderjava/MainActivity.java
+++ app/src/main/java/com/grogers/seedspreaderjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.grogers.seedspreaderjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View v = findViewById(R.id.textViewMotd);
+        TextView vv = (TextView) v;
+        vv.setText("This is message of the day");
     }
 }
